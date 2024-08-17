cdd9b8513124a186715a7b4fa2dd3ed2b1a5a331
thaonoa1425 <phuongthao0104sl@gmail.com>
2024-08-17 16:48:16 +0700

    init project

---
+++ src/test/java/edu/uneti/stem/StemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uneti.stem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
